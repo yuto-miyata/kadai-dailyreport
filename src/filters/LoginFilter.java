bb87998fa8ec9f5183bd06439a671da257972e5c
yuto.miyata <yuto.miyata@forumeng.co.jp>
2018-06-01 16:27:24 +0900

    Add Report class

--- src/filters/LoginFilter.java
+++ src/filters/LoginFilter.java
@@ -52,7 +52,7 @@ public class LoginFilter implements Filter {
 		            return;
 		        }
 
-		        if(servlet_path.matches("/employee.*) && e.getAdmin_flag() == 0")) {
+		        if(servlet_path.matches("/employee.*") && e.getAdmin_flag() == 0) {
 		            ((HttpServletResponse)response).sendRedirect(context_path + "/");
 		            return;
 		        }
