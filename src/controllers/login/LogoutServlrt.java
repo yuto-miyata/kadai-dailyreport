d1e84f01aa5aca12d2f418df6d331042e5dc3d90
yuto.miyata <yuto.miyata@forumeng.co.jp>
2018-06-01 13:09:25 +0900

    Add LoginFilter

---
+++ src/controllers/login/LogoutServlrt.java
@@ -0,0 +1,36 @@
+package controllers.login;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlrt
+ */
+@WebServlet("/logout")
+public class LogoutServlrt extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlrt() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().removeAttribute("login_employee");
+
+		request.getSession().setAttribute("flush", "ログアウトしました。");
+		response.sendRedirect(request.getContextPath() + "/login");
+	}
+
+}
