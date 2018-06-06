f8e42cafc833f2c11144db8bbc534e54d2106682
yuto.miyata <yuto.miyata@forumeng.co.jp>
2018-06-06 10:46:10 +0900

    Add Report show/edit

--- src/controllers/reports/ReportsShowServlet.java
+++ src/controllers/reports/ReportsShowServlet.java
@@ -1,11 +1,17 @@
 package controllers.reports;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Report;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class ReportsShowServlet
@@ -13,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/reports/show")
 public class ReportsShowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,8 +32,17 @@ public class ReportsShowServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		EntityManager em = DBUtil.createEntityManager();
+
+		Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+
+		em.close();
+
+		request.setAttribute("report", r);
+		request.setAttribute("_token", request.getSession().getId());
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
+		rd.forward(request, response);
 	}
 
 }
